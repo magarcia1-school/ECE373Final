c74563a72d0e1ca5615cb82b312a96fc8e821ab7
magarcia1 <magarcia1@email.arizona.edu>
2015-12-02 13:04:32 -0700

    MainScreen doing something

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,4 +1,54 @@
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class MainScreen {
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
+/**
+ * This class demonstrates how to load an Image from an external file
+ */
+public class MainScreen extends Component {
+          
+    BufferedImage img;
+
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+
+    public MainScreen() {
+       try {
+           img = ImageIO.read(new File("MainScreen.jpg"));
+       } catch (IOException e) {
+       }
+
+    }
+
+    public Dimension getPreferredSize() {
+        if (img == null) {
+             return new Dimension(100,100);
+        } else {
+           return new Dimension(img.getWidth(null), img.getHeight(null));
+       }
+    }
+
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame("Load Image Sample");
+            
+        f.addWindowListener(new WindowAdapter(){
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+
+        f.add(new MainScreen());
+        f.pack();
+        f.setVisible(true);
+    }
 }
