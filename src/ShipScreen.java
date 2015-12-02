ef05a258177c8940eef23a4c9c8a4a2dd02daf4d
Phillip Showers <pshowers73@aol.com>
2015-12-02 14:09:01 -0700

    Trying to add .png file

--- src/ShipScreen.java
+++ src/ShipScreen.java
@@ -1,4 +1,13 @@
 public class ShipScreen
 {
-
+	public BufferedImage loadXWing()
+	{
+		//Create a BufferedImage object
+		BufferedImage img = null;
+		//Open the XWingFighter.jpg picture
+		try {
+			img = ImageIO.read(new File("XWingFighter.png"));
+		} catch (IOException e) {
+		}
+	}
 }
